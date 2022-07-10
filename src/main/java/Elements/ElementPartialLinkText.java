54d9c104f713c294f790e744511d42e6b0022162
Rene Allende <rene.allende15@gmail.com>
2022-07-10 10:20:00 -0400

    Se crean las clases ElementPartialLinkText, ElementXpath, ElementTag, ElementClass

---
+++ src/main/java/Elements/ElementPartialLinkText.java
@@ -0,0 +1,25 @@
+package Elements;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ElementPartialLinkText {
+
+	public static void main(String[] args) throws InterruptedException {
+		// Variables para usar el binario
+		String DriverNavegador = "webdriver.chrome.driver";
+		String RutaDriver = "C:\\Rene\\Binarios\\ChromeDirvers\\chromedriver(103.0.5060.53).exe";
+		String url ="https://www.google.com/";
+		WebDriver driver;
+		System.setProperty(DriverNavegador, RutaDriver);
+		driver = new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		
+		
+		Thread.sleep(5000);
+		driver.quit();
+
+	}
+
+}
