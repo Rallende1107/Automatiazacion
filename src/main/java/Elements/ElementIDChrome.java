9f909ccd9c4ad9667685aab8ab23b598abd36733
Rene Allende <rene.allende15@gmail.com>
2022-07-10 10:06:59 -0400

    "Se Añande la expresion Thread en ElementIDChrome"

--- src/main/java/Elements/ElementIDChrome.java
+++ src/main/java/Elements/ElementIDChrome.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ElementIDChrome {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// Variables para usar el binario
 		String DriverNavegador = "webdriver.chrome.driver";
 		String RutaDriver = "C:\\Rene\\Binarios\\ChromeDirvers\\chromedriver(103.0.5060.53).exe";
@@ -15,6 +15,10 @@ public class ElementIDChrome {
 		driver = new ChromeDriver();
 		driver.get(url);
 		driver.manage().window().maximize();
+		
+		
+		Thread.sleep(5000);
+		driver.quit();
 
 	}
 
