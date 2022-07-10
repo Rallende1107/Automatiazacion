8d93203e2222c4dab443c5ed0b5f7a0a9064c2f9
Rene Allende <rene.allende15@gmail.com>
2022-07-10 05:29:38 -0400

    Trabajando y abriendo Phanthom JS, se agrega dependencia phanthom JS

--- src/main/java/Test/OpenPhantonJS.java
+++ src/main/java/Test/OpenPhantonJS.java
@@ -1,10 +1,26 @@
 package Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
 public class OpenPhantonJS {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
+		// Instanciando el WebDriver
+		WebDriver driver;
+		
+		// Ruta del binario
+		System.setProperty("phantomjs.binary.path", "C:\\Rene\\Binarios\\PhantomJS\\phantomjs-2.1.1\\bin\\phantomjs.exe");
+		
+		driver  = new PhantomJSDriver();
+		
+		System.out.println("Se abrip en navegador");
+		
+		driver.get("https://www.google.com/");
+		Thread.sleep(5000)	;
+		driver.quit();		
 	}
 
 }
